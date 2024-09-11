bf95abaf257c761556d9ca1323b22fa9e2fe90c3
Petersix <150849737+peterarcuri@users.noreply.github.com>
2024-09-10 23:32:22 -0400

    edit nested for loop

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ console.log('======= BUBBLESORT ========');
 function bubbleSort(arr) {
 
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length -i - 1; j++) {
+        for (let j = 0; j < arr.length -1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
